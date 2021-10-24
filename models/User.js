2bf9d59246d79323ad72e5b2973f50da5c41ecb6
Tutor <tutor.michael78@gmail.com>
2021-10-24 17:06:39 -0500

    added user model

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: {
+                // uses a regex to validate that the input is an email
+                validator: function(v) {
+                  return /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(v);
+                },
+                message: props => `${props.email} is not a valid email!`
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+// virtual containing the amount of friends
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = modal('User', UserSchema);
+
+module.exports = User;
